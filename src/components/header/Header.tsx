2e893505740e0ed28d2be6cba94496f3d7722f56
Kim Hofmann <kim.hofmann@ict-campus.net>
2026-07-07 14:58:56 +0200

    add header display none if pathname '/'

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,20 @@
+"use client"
 import Navigation from "../navigation/Navigation";
 import styles from "./Header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function HeaderComponent() {
-    
-    return(
+
+    const HIDDEN_ROUTES = ["/"];
+    const pathname = usePathname();
+
+    if (HIDDEN_ROUTES.includes(pathname)) {
+        return null;
+    } else {
+        return(
         <header className={styles.header}>
             <Navigation/>
         </header>
-    )
+    );
+    }
 }
