c277aa4d5498e35f0f42bf785f38a3d1d5b24b01
Kim Hofmann <kim.hofmann@ict-campus.net>
2026-07-07 08:01:10 +0200

    add next js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Note Tracker - For Students",
+  description: "With this webapp you have an overview of your school note.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
