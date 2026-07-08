8a6124113a00ef97f4373728bbfc469afa4d5184
Kim Hofmann <kim.hofmann@ict-campus.net>
2026-07-08 08:29:59 +0200

    add backgroup image for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function Landing() {
-
   return (
     <div className={styles.page}>
+      <div className={styles.img_container}>
+        <Image src={"/assets/landingpage_bg.jpg"} alt="backgroup image" width={1600} height={900}></Image>
+      </div>
       <div className={styles.title}>
         <h1>Keep track of your grades</h1>
         <h3>Enter your grades and track your progress.</h3>
@@ -25,7 +27,39 @@ export default function Landing() {
               width={64}
               height={64}
             ></Image>
-            <p>Enter your grades and always keep track of your average grade.</p>
+            <p>
+              Enter your grades and always keep track of your average grade.
+            </p>
+          </div>
+        </div>
+        <div className={styles.feature}>
+          <h4>Average Calculation</h4>
+          <div>
+            <Image
+              src={"/assets/edit.png"}
+              alt="tracking"
+              width={64}
+              height={64}
+            ></Image>
+            <p>
+              See your overall grade average update automatically as you add new
+              grades.
+            </p>
+          </div>
+        </div>
+        <div className={styles.feature}>
+          <h4>Export Grades</h4>
+          <div>
+            <Image
+              src={"/assets/export.png"}
+              alt="tracking"
+              width={64}
+              height={64}
+            ></Image>
+            <p>
+              Export your grades anytime as a PDF or spreadsheet to share or
+              keep for your records.
+            </p>
           </div>
         </div>
       </div>
